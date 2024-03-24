528653db06ecd99fa3291d70d5a0f377c0893a63
amatheusamaral <164792874+amatheusamaral@users.noreply.github.com>
2024-03-24 18:01:25 -0300

    Add files via upload

---
+++ EXERCICIOS/Programa.java
@@ -0,0 +1,28 @@
+public class Programa {
+
+    public static void main(String[] args) {
+
+        Exercicio1.executar();
+        Exercicio2.executar();
+        Exercicio3.executar();
+        Exercicio4.executar();
+        Exercicio5.executar();
+        Exercicio6.executar();
+        Exercicio7.executar();
+        Exercicio8.executar();
+        Exercicio9.executar();
+        Exercicio10.executar();
+        Exercicio11.executar();
+        Exercicio12.executar();
+        Exercicio13.executar();
+        Exercicio14.executar();
+        Exercicio15.executar();
+        Exercicio16.executar();
+        Exercicio17.executar();
+        Exercicio18.executar();
+        Exercicio19.executar();
+        Exercicio20.executar();
+        
+    }
+
+}
