ee8ee3fd765395ed21ab5eb3ceedc97a2fb70fd4
amatheusamaral <164792874+amatheusamaral@users.noreply.github.com>
2024-03-24 17:54:24 -0300

    Add files via upload

---
+++ Exercicio19.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Exercicio19 {
+
+    public static void executar() {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Digite o raio do cilindro: ");
+        double raio = scanner.nextDouble();
+        
+        System.out.print("Digite a altura do cilindro: ");
+        double altura = scanner.nextDouble();
+        
+        double volume = 3.14 * Math.pow(raio, 2) * altura;
+        
+        System.out.println("O volume do cilindro é: " + volume);
+    }
+
+}
