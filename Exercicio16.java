df6814651862926268fddd437c778af48ebc2191
amatheusamaral <164792874+amatheusamaral@users.noreply.github.com>
2024-03-24 17:53:52 -0300

    Add files via upload

---
+++ Exercicio16.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Exercicio16 {
+
+    public static void executar() {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Digite a primeira nota: ");
+        double nota1 = scanner.nextDouble();
+        
+        System.out.print("Digite a segunda nota: ");
+        double nota2 = scanner.nextDouble();
+        
+        System.out.print("Digite a terceira nota: ");
+        double nota3 = scanner.nextDouble();
+        
+        double media = (nota1 + nota2 + nota3) / 3;
+        
+        System.out.println("A média aritmética das notas é: " + media);
+    }
+
+}
